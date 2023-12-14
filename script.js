b126acc5d1a11f3c94bca679c8875bed2618f766
Nila Chandra Laishram <nilalaishram@gmail.com>
2023-12-14 22:25:21 +0530

    first day final commit

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
 //GSAP animation
-gsap.from('nav img, nav h3,nav button',{
+let tl = gsap.timeline();
+
+tl.from('nav img, nav h3,nav button',{
     y:-100,
     duration: 1,
-    delay: 1,
+    delay: 0.5,
     opacity: 0,
-    stagger: 0.5
+    stagger: 0.2
+})
+
+tl.from('main h1',{
+    y: 100,
+    opacity: 0,
+    stagger: 0.35
+})
+
+tl.from('main>img',{
+    scale: 0,
+    opacity: 0,
+    stagger: 0.35
+})
+tl.from('h5',{
+    y: 40,
+    repeat: -1,
+    opacity: 0,
+    duration: 1,
+    yoyo: true,
 })
